1d9abd716789aa4578d5400591b389d700843799
azedqsaze <sadm3979@gmail.com>
2024-03-13 23:04:27 +0000

    add sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
+import Sidebar from "@/components/Sidebar";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = FontSans({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +19,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="min-h-screen flex ">
+            <Sidebar />
+            <main>{children}</main>
+          </div>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { navItems } from "@/lib/navItems";
+import Link from "next/link";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "./ui/hover-card";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+function Sidebar() {
+  const pathname = usePathname();
+
+  return (
+    <div className="min-h-screen border-r border-gray-200 w-14 flex flex-col items-center justify-center gap-8">
+      {navItems.map((item) => (
+        <div key={item.name}>
+          <HoverCard>
+            <HoverCardTrigger asChild>
+              <Link
+                href={item.href ? item.href : ""}
+                className={cn("text-gray-500", {
+                  "text-violet-600": pathname === item.href,
+                })}
+              >
+                {item.icon}
+              </Link>
+            </HoverCardTrigger>
+            <HoverCardContent
+              side="right"
+              className="text-sm text-center  bg-black text-white w-fit p-0 px-2 py-1"
+            >
+              {item.name}
+            </HoverCardContent>
+          </HoverCard>
+        </div>
+      ))}
+    </div>
+  );
+}
+export default Sidebar;
